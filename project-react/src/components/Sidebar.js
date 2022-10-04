c7e6d107655dba67205d4b61469b2390d78e46a1
waleedtarbosh <waleedtarbush@gmail.com>
2022-10-04 15:33:17 +0300

    remove bulid folder and create constant file

--- project-react/src/components/Sidebar.js
+++ project-react/src/components/Sidebar.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import {SidebarData} from '../data/SidebarData'
 import { NavLink } from "react-router-dom";
-
+import { activeLink , normalLink } from "./Sidebarconstant";
 const Sidebar = () => {
-    const activeLink = 'hover:bg-red-500 mt-7 pl-7 w-full h-14 flex justify-start items-center text-white text-2xl space-x-1 font-bold bg-red-500'
-    const normalLink = 'hover:bg-red-500 pl-7 mt-7 w-full h-14 flex justify-start items-center text-white text-2xl space-x-1 font-bold'
-
+    
   return (
     <React.Fragment>
     <section>
